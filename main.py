f9b33680c536791523c1acc4d4ea35dfd94bcd37
Ayush Bhardwaj <62342389+AB270@users.noreply.github.com>
2021-07-20 17:50:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import cv2
 import time
 import numpy as np
 
-video = cv2.VideoCapture(0)
-image = cv2.imread("TajMahal.png")
+vid = cv2.VideoCapture(0)
+img = cv2.imread("tajMahal.jpeg")
 
 while True:
-    ret,frame = video.read()
+    ret,frame = vid.read()
     print(frame)
     frame = cv2.resize(frame,(640,480))
-    image = cv2.resize(image,(640,480))
+    img = cv2.resize(img,(640,480))
     u_black = np.array([104,153,70])
     l_black = np.array([30,30,0])
     mask = cv2.inRange(frame,l_black,u_black)
     res = cv2.bitwise_and(frame,frame,mask = mask)
     f = frame-res
-    f = np.where(f ==0,image,f)
-    cv2.imshow("video",frame)
+    f = np.where(f ==0,img,f)
+    cv2.imshow("vid",frame)
     cv2.imshow("mask",f)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
